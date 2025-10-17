144eb5885050ee5f72be6ee3c9d5a370323d66db
shank-codes <shashankhiremat22@gmail.com>
2025-10-17 19:13:32 +0530

    grid size glitch fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${poppins.variable} antialiased`}>{children}</body>
     </html>
   );
